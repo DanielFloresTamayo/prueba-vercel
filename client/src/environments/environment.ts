c9ab9fc2c0f9a5d2f47a7f699e425783650047ef
DanielFloresTamayo <danielflorestamayo@gmail.com>
2025-12-09 22:48:26 -0500

    Actualizaciones para apuntar a Railway

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000' // JSON Server
+  apiUrl: 'https://grateful-essence-production-2233.up.railway.app' // JSON Server
 };
