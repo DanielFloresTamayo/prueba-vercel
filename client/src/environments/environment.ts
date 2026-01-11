8a84d1a1c21db3d4f100e59d79b3038e89e700c8
DanielFloresTamayo <danielflorestamayo@gmail.com>
2026-01-11 03:52:23 -0500

    Cambio de URL Jsonserver

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://grateful-essence-production-2233.up.railway.app' // JSON Server
+  apiUrl: 'https://mi-json-backend.onrender.com/' // JSON Server
 };
